07584c69d9feb2306f9635d2ec84c8c91a05c2bd
wilson30139 <43327415+wilson30139@users.noreply.github.com>
2021-12-09 01:03:56 +0800

    Add files via upload

---
+++ 2021_12_09/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Page page = new Page();
+            page.Login("123456", "123Eric");
+        }
+    }
+
+    public class Page
+    {
+        public void Login(string empid, string password)
+        {
+            EmployeeDB employeeDB = new EmployeeDB();
+            employeeDB.GetEmployee(empid);
+            Employee employee = new Employee();
+            var test = employee.Validate(password);
+        }
+    }
+
+    public class Employee
+    {
+        public bool Validate(string password)
+        {
+            return true;
+        }
+    }
+
+    public class EmployeeDB
+    {
+        public Employee GetEmployee(string empid)
+        {
+            Employee e = new Employee();
+            return e;
+        }
+    }
+}
